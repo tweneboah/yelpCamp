58d18a4e7ba0e2072cf5a72834f75e1058f36990
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-08-12 16:06:27 +0800

    V14 -Update: Middleware moved to its own file

---
+++ yelpcamp-v15/routes/campgrounds.js
@@ -0,0 +1,128 @@
+const express = require('express');
+const router = express.Router();
+const Campground = require('../models/campground')
+const middleware = require('../middleware/index')
+
+
+//GET FORM
+router.get("/new", middleware.isLogin, function(req, res){
+ 
+ res.render("campgrounds/new.ejs"); 
+});
+
+
+//==================================
+//INDEX PAGE -> Show all campgrounds
+//==================================
+
+router.get('/', (req, res) => {
+ //Get All campgrounds from DB
+ 
+ Campground.find({}, (err, allCampgrounds)=> {
+     if(err){
+         console.log(err)
+     }else {
+         
+         res.render('campgrounds/index', {campgrounds: allCampgrounds, currentUser: req.user})
+     }
+ })
+})
+
+
+//============================
+// CREATE CAMPGROUNDS
+//============================
+
+router.post("/" , middleware.isLogin ,function(req, res){
+ // get data from form and add to campgrounds array
+ var name = req.body.name;
+ var image = req.body.image;
+ let desc = req.body.description
+
+ //This must be the same as field in the model
+ let author = {
+     id: req.user.id,
+     username: req.user.username
+ }
+ var newCampground = {name: name, image: image, description: desc, author: author}
+
+ //Create a new campground and save to DB
+ Campground.create(newCampground, (err, newlyCreated) => {
+     if(err){
+         //console.log(err)
+     }else {
+         //Redirect back to campground page
+         console.log('Newly Created Cam', newlyCreated)
+         res.redirect('/campgrounds')
+     }
+ })
+ 
+});
+
+//=================
+// SHOW MORE INFO
+//=============
+
+router.get('/:id', (req, res) => {
+ Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+     if(err){
+         console.log(err);
+     } else {
+         //render show template with that campground
+         res.render("campgrounds/show", {campground: foundCampground});
+     }
+ });
+});
+
+
+//==============
+//EDIT CAMPGROUND AND UPDATE
+//=============
+
+//get the edit form
+router.get('/:id/edit', function(req, res) {
+
+        Campground.findById(req.params.id, (err, foundCampground) => {
+            res.render('campgrounds/edit', {campground: foundCampground}) 
+
+        })            
+ })
+
+//Editing logic
+router.put('/:id', (req, res) => {
+    //find the campground you want to update and then retrieve the data from the form you want to update. Because of that we will build the data coming from the edit form and pass it as a second arguemnt to findByIdAndUpdate
+    let campgroundsToBeUpdated = {
+        name: req.body.name,
+        image: req.body.image,
+        description: req.body.description
+    }
+    Campground.findByIdAndUpdate(req.params.id, campgroundsToBeUpdated, (err, updatedCampground) => {
+        if(err){
+            console.log(err)
+        }else {
+            //res.send('EDIT ME')
+            console.log(updatedCampground);
+            res.redirect('/campgrounds/' + req.params.id)
+        }
+    })
+})
+
+
+//==============
+// DELETE
+//=========
+
+router.delete("/:id",middleware.isLogin ,function(req, res){
+    Campground.findByIdAndRemove(req.params.id, function(err){
+       if(err){
+           res.redirect("/campgrounds");
+       } else {
+           res.redirect("/campgrounds");
+       }
+    });
+ });
+
+
+ 
+module.exports = router;
+
---
+++ yelpcamp-v15/routes/index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const User = require('../models/user')
+
+
+//middleware
+function isLoggedIn(req, res, next){
+ if(req.isAuthenticated()){
+     return next();
+ }
+ req.flash('error', 'Please login first')
+ res.redirect("/login");
+}
+
+//============
+ // REGISTER USER
+ //Show form
+ //===========
+ router.get('/register', (req, res) => {
+  res.render('register')
+});
+
+//Registration logic
+
+router.post('/register', (req, res) => {
+  let newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, (err, user) => {
+      if(err){
+          console.log('Registration Error', err.message);
+          return res.render('register')
+      }else {
+          passport.authenticate('lpcal')(req, res, () =>{
+             res.redirect('/campgrounds')
+          });
+      }
+  })
+})
+
+
+//Login form
+router.get('/login', (req, res) => {
+  res.render('login', {message: req.flash('error')})
+})
+
+//Login logic
+router.post('/login',passport.authenticate('local', {
+  successRedirect: '/campgrounds',
+  failureRedirect: '/login'
+}) , (req, res) => {
+  
+});
+
+//logout route
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/campgrounds')
+})
+
+
+
+
+
+module.exports = router;
+
+
