940d173729ee165b7bba1b5b72112031af1f48f5
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-08-12 14:05:27 +0800

    V14 - delete without authorization: any user can delete any comment

---
+++ yelpcamp-v14/models/campground.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+let campgroundSchema = new mongoose.Schema({
+   name: String,
+   image: String,
+   description: String,
+   createdAt: {
+      type: Date,
+      default: Date.now()
+   },
+   comments: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Comment"
+      }
+   ],
+   author: {
+      id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: 'User'
+      },
+      username: String, 
+   }
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
